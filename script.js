2ed8c959f13c06291c401e4212322732dafa49bd
salohiddinteshaboyev30-dot <salohiddinteshaboyev30@gmail.com>
2026-03-07 07:30:09 -0800

    izoh

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
+let people = [
+
+{
+name:"Alisher Navoiy",
+img:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Alisher_Navoi.jpg",
+link:"https://uz.wikipedia.org/wiki/Alisher_Navoiy"
+},
+
+{
+name:"Amir Temur",
+img:"https://upload.wikimedia.org/wikipedia/commons/7/72/Amir_Timur_reconstruction.jpg",
+link:"https://uz.wikipedia.org/wiki/Amir_Temur"
+},
+
+{
+name:"Ibn Sino",
+img:"https://upload.wikimedia.org/wikipedia/commons/1/1e/Avicenna_Portrait.jpg",
+link:"https://uz.wikipedia.org/wiki/Abu_Ali_ibn_Sino"
+},
+
+{
+name:"Mirzo Ulug‘bek",
+img:"https://upload.wikimedia.org/wikipedia/commons/7/77/Ulugh_Beg.jpg",
+link:"https://uz.wikipedia.org/wiki/Mirzo_Ulug%CA%BBbek"
+},
+
+{
+name:"Zahiriddin Bobur",
+img:"https://upload.wikimedia.org/wikipedia/commons/5/5e/Babur.jpg",
+link:"https://uz.wikipedia.org/wiki/Zahiriddin_Muhammad_Bobur"
+}
+
+];
+
 let container = document.getElementById("container");
-let search = document.getElementById("search");
-
-fetch("data/bobolar.json")
-.then(res => res.json())
-.then(data => {
-
-show(data);
-
-search.addEventListener("keyup", ()=>{
-
-let value = search.value.toLowerCase();
-
-let filtered = data.filter(person =>
-person.name.toLowerCase().includes(value)
-);
-
-show(filtered);
-
-});
-
-});
 
 function show(list){
 
 container.innerHTML="";
 
-list.forEach(person=>{
+list.forEach(p=>{
 
-let card = document.createElement("div");
+let card=document.createElement("div");
 card.className="card";
 
-card.innerHTML = `
-<img src="images/${person.image}">
-<div class="name">${person.name}</div>
+card.innerHTML=`
+<img src="${p.img}">
+<div class="name">${p.name}</div>
 `;
 
-card.onclick = ()=>{
-window.open(person.link);
+card.onclick=()=>{
+window.open(p.link);
 }
 
 container.appendChild(card);
@@ -44,3 +57,17 @@ container.appendChild(card);
 });
 
 }
+
+show(people);
+
+document.getElementById("search").addEventListener("keyup",function(){
+
+let value=this.value.toLowerCase();
+
+let filtered=people.filter(p =>
+p.name.toLowerCase().includes(value)
+);
+
+show(filtered);
+
+});
