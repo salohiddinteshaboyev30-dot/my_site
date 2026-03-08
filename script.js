6f032d867818fc3aef0f40f898ba138b024a892f
salohiddinteshaboyev30-dot <salohiddinteshaboyev30@gmail.com>
2026-03-07 22:11:55 -0800

    izoh

--- script.js
+++ script.js
@@ -1,31 +1,127 @@
-let container = document.getElementById("container")
+const people=[
 
-function show(list){
+{
+name:"Alisher Navoiy",
+img:"https://upload.wikimedia.org/wikipedia/commons/6/6a/Alisher_Navoi_2.jpg",
+info:"Buyuk shoir va davlat arbobi."
+},
 
-container.innerHTML=""
+{
+name:"Amir Temur",
+img:"https://upload.wikimedia.org/wikipedia/commons/1/1c/Tamerlan.jpg",
+info:"Temuriylar imperiyasi asoschisi."
+},
 
-list.forEach(p=>{
+{
+name:"Mirzo Ulugbek",
+img:"https://upload.wikimedia.org/wikipedia/commons/3/3a/Ulugh_Beg.jpg",
+info:"Mashhur astronom va matematik."
+},
 
-container.innerHTML+=`
-<div class="card">
-<h3>${p.name}</h3>
-</div>
-`
+{
+name:"Zahiriddin Bobur",
+img:"https://upload.wikimedia.org/wikipedia/commons/0/0e/Babur.jpg",
+info:"Boburiylar imperiyasi asoschisi."
+},
 
-})
+{
+name:"Ibn Sino",
+img:"https://upload.wikimedia.org/wikipedia/commons/1/1a/Avicenna_Portrait.jpg",
+info:"Mashhur tabib va olim."
+}
+
+];
+
+const grid=document.getElementById("grid");
+
+function highlight(text,search){
+
+if(!search) return text;
+
+const reg=new RegExp(search,"gi");
+
+return text.replace(reg,match=>`<span class="highlight">${match}</span>`);
 
 }
 
-show(people)
+function render(list,search=""){
 
-document.getElementById("search").addEventListener("input",function(){
+grid.innerHTML="";
 
-let q=this.value.toLowerCase()
+list.forEach(p=>{
 
-let filtered=people.filter(p =>
-p.name.toLowerCase().includes(q)
-)
+let card=document.createElement("div");
 
-show(filtered)
+card.className="card";
 
-})
+card.innerHTML=`
+
+<img src="${p.img}">
+<h3>${highlight(p.name,search)}</h3>
+
+`;
+
+card.onclick=()=>openModal(p);
+
+grid.appendChild(card);
+
+});
+
+}
+
+render(people);
+
+document.getElementById("search").addEventListener("input",e=>{
+
+let text=e.target.value.toLowerCase();
+
+let filtered=people.filter(p=>p.name.toLowerCase().includes(text));
+
+render(filtered,text);
+
+});
+
+function openModal(p){
+
+document.getElementById("modal").style.display="flex";
+
+document.getElementById("modalName").innerText=p.name;
+
+document.getElementById("modalInfo").innerText=p.info;
+
+}
+
+function closeModal(){
+
+document.getElementById("modal").style.display="none";
+
+}
+
+document.getElementById("voiceBtn").onclick=function(){
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if(!SpeechRecognition){
+alert("Chrome brauzer kerak");
+return;
+}
+
+const recognition=new SpeechRecognition();
+
+recognition.lang="uz-UZ";
+
+recognition.onresult=function(e){
+
+let text=e.results[0][0].transcript;
+
+document.getElementById("search").value=text;
+
+let filtered=people.filter(p=>p.name.toLowerCase().includes(text.toLowerCase()));
+
+render(filtered,text);
+
+}
+
+recognition.start();
+
+}
